c3dd9a4a71e6181cc228cad258858c5e4caf62dc
Nurudeen <ajayinurudeen.wale@gmail.com>
2022-12-17 22:22:22 +0000

    Second test

--- server/database/factories/ShipperFactory.php
+++ server/database/factories/ShipperFactory.php
@@ -17,7 +17,6 @@ class ShipperFactory extends Factory
     public function definition()
     {
         return [
-            'company_name' => $this->faker->company(),
             'address' => $this->faker->address(),
         ];
     }
